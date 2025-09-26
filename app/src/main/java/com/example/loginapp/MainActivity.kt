872da3ac02383584f621ae1370bd508622ca65fe
Kuripipeer <mchristianaxel@gmail.com>
2025-09-26 01:24:46 -0600

    implement functional login screen with styled TextFields and navigation

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -6,19 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,12 +41,22 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.loginapp.ui.theme.Background
+import com.example.loginapp.ui.theme.TextFieldColor
+import com.example.loginapp.ui.theme.Lock
+import com.example.loginapp.ui.theme.Lock_open_right
 import com.example.loginapp.ui.theme.LoginAppTheme
 import com.example.loginapp.ui.theme.LoginScreenRoute
 import com.example.loginapp.ui.theme.RocketTakeoff
@@ -54,10 +75,10 @@ class MainActivity : ComponentActivity() {
                         startDestination = LoginScreenRoute
                     ){
                         composable <LoginScreenRoute> {
-                            Greeting(navController)
+                            Login(navController)
                         }
                         composable <SingUpScreenRoute> {
-
+                            Login(navController)
                         }
                     }
                 }
@@ -67,7 +88,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(navController: NavController) {
+fun Login(navController: NavController) {
     var email by remember {
         mutableStateOf("")
     }
@@ -117,11 +138,108 @@ fun Greeting(navController: NavController) {
                     .weight(2f)
                     .fillMaxWidth()
                     .clip(RoundedCornerShape(topStart = 60.dp))
-                    .background(Color.White)
-                    .padding(horizontal = 24.dp, vertical = 28.dp),
+                    .background(Background)
+                    .padding(horizontal = 38.dp, vertical = 38.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
+                Column (
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text = "Login",
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold
+                    )
 
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text(text = "Email", color = Color.Black) },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 40.dp, bottom = 28.dp)
+                            .height(80.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedIndicatorColor = Color.White,
+                            unfocusedContainerColor = TextFieldColor,
+                            focusedContainerColor = TextFieldColor,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedTextColor = Color.LightGray,
+                            disabledIndicatorColor = Color.Transparent
+                        )
+                    )
+
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text(text = "Password", color = Color.Black) },
+                        singleLine = true,
+                        trailingIcon = {
+                            IconButton(onClick = { showPassword = !showPassword }) {
+                                Icon(
+                                    imageVector = if (showPassword) Lock_open_right else Lock,
+                                    contentDescription = if (showPassword) "Ocultar" else "Mostrar"
+                                )
+                            }
+                        },
+                        visualTransformation = if (showPassword) VisualTransformation.None
+                        else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password,
+                            imeAction = ImeAction.Done
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(80.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedIndicatorColor = Color.White,
+                            unfocusedContainerColor = TextFieldColor,
+                            focusedContainerColor = TextFieldColor,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedTextColor = Color.LightGray,
+                            disabledIndicatorColor = Color.Transparent
+                        )
+                    )
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 28.dp)
+                            .height(60.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Black,
+                            contentColor = Color.White
+                        ),
+                        shape = RoundedCornerShape(
+                            topStart = 16.dp,
+                            bottomStart = 16.dp,
+                            bottomEnd = 16.dp
+                        )
+                    ) {
+                        Text(
+                            text = "Login",
+                            fontSize = 18.sp
+                        )
+                    }
+                }
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text = "Don't have any account? Sign Up",
+                        modifier = Modifier
+                            .clickable {
+                                navController.navigate(SingUpScreenRoute)
+                            }
+                    )
+                }
             }
         }
     }
@@ -131,6 +249,6 @@ fun Greeting(navController: NavController) {
 @Composable
 fun GreetingPreview() {
     LoginAppTheme {
-        Greeting(navController = rememberNavController())
+        Login(navController = rememberNavController())
     }
 }
--- app/src/main/java/com/example/loginapp/ui/theme/CustomIcons.kt
+++ app/src/main/java/com/example/loginapp/ui/theme/CustomIcons.kt
@@ -106,3 +106,134 @@ val RocketTakeoff: ImageVector
 
 private var _RocketTakeoff: ImageVector? = null
 
+val Lock_open_right: ImageVector
+    get() {
+        if (_Lock_open_right != null) return _Lock_open_right!!
+
+        _Lock_open_right = ImageVector.Builder(
+            name = "Lock_open_right",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000))
+            ) {
+                moveTo(240f, 800f)
+                horizontalLineToRelative(480f)
+                verticalLineToRelative(-400f)
+                horizontalLineTo(240f)
+                close()
+                moveToRelative(240f, -120f)
+                quadToRelative(33f, 0f, 56.5f, -23.5f)
+                reflectiveQuadTo(560f, 600f)
+                reflectiveQuadToRelative(-23.5f, -56.5f)
+                reflectiveQuadTo(480f, 520f)
+                reflectiveQuadToRelative(-56.5f, 23.5f)
+                reflectiveQuadTo(400f, 600f)
+                reflectiveQuadToRelative(23.5f, 56.5f)
+                reflectiveQuadTo(480f, 680f)
+                moveTo(240f, 800f)
+                verticalLineToRelative(-400f)
+                close()
+                moveToRelative(0f, 80f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(160f, 800f)
+                verticalLineToRelative(-400f)
+                quadToRelative(0f, -33f, 23.5f, -56.5f)
+                reflectiveQuadTo(240f, 320f)
+                horizontalLineToRelative(280f)
+                verticalLineToRelative(-80f)
+                quadToRelative(0f, -83f, 58.5f, -141.5f)
+                reflectiveQuadTo(720f, 40f)
+                reflectiveQuadToRelative(141.5f, 58.5f)
+                reflectiveQuadTo(920f, 240f)
+                horizontalLineToRelative(-80f)
+                quadToRelative(0f, -50f, -35f, -85f)
+                reflectiveQuadToRelative(-85f, -35f)
+                reflectiveQuadToRelative(-85f, 35f)
+                reflectiveQuadToRelative(-35f, 85f)
+                verticalLineToRelative(80f)
+                horizontalLineToRelative(120f)
+                quadToRelative(33f, 0f, 56.5f, 23.5f)
+                reflectiveQuadTo(800f, 400f)
+                verticalLineToRelative(400f)
+                quadToRelative(0f, 33f, -23.5f, 56.5f)
+                reflectiveQuadTo(720f, 880f)
+                close()
+            }
+        }.build()
+
+        return _Lock_open_right!!
+    }
+
+private var _Lock_open_right: ImageVector? = null
+
+val Lock: ImageVector
+    get() {
+        if (_Lock != null) return _Lock!!
+
+        _Lock = ImageVector.Builder(
+            name = "Lock",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000))
+            ) {
+                moveTo(240f, 880f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(160f, 800f)
+                verticalLineToRelative(-400f)
+                quadToRelative(0f, -33f, 23.5f, -56.5f)
+                reflectiveQuadTo(240f, 320f)
+                horizontalLineToRelative(40f)
+                verticalLineToRelative(-80f)
+                quadToRelative(0f, -83f, 58.5f, -141.5f)
+                reflectiveQuadTo(480f, 40f)
+                reflectiveQuadToRelative(141.5f, 58.5f)
+                reflectiveQuadTo(680f, 240f)
+                verticalLineToRelative(80f)
+                horizontalLineToRelative(40f)
+                quadToRelative(33f, 0f, 56.5f, 23.5f)
+                reflectiveQuadTo(800f, 400f)
+                verticalLineToRelative(400f)
+                quadToRelative(0f, 33f, -23.5f, 56.5f)
+                reflectiveQuadTo(720f, 880f)
+                close()
+                moveToRelative(0f, -80f)
+                horizontalLineToRelative(480f)
+                verticalLineToRelative(-400f)
+                horizontalLineTo(240f)
+                close()
+                moveToRelative(240f, -120f)
+                quadToRelative(33f, 0f, 56.5f, -23.5f)
+                reflectiveQuadTo(560f, 600f)
+                reflectiveQuadToRelative(-23.5f, -56.5f)
+                reflectiveQuadTo(480f, 520f)
+                reflectiveQuadToRelative(-56.5f, 23.5f)
+                reflectiveQuadTo(400f, 600f)
+                reflectiveQuadToRelative(23.5f, 56.5f)
+                reflectiveQuadTo(480f, 680f)
+                moveTo(360f, 320f)
+                horizontalLineToRelative(240f)
+                verticalLineToRelative(-80f)
+                quadToRelative(0f, -50f, -35f, -85f)
+                reflectiveQuadToRelative(-85f, -35f)
+                reflectiveQuadToRelative(-85f, 35f)
+                reflectiveQuadToRelative(-35f, 85f)
+                close()
+                moveTo(240f, 800f)
+                verticalLineToRelative(-400f)
+                close()
+            }
+        }.build()
+
+        return _Lock!!
+    }
+
+private var _Lock: ImageVector? = null
+
